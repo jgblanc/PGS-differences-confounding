7466ae7f145e84772469586de67965c58016138b
jgblanc <jgblanc@uchicago.edu>
2024-02-08 10:19:47 -0600

    t

---
+++ code/plot/concat_q_rep_SimpleGrid.R
@@ -0,0 +1,47 @@
+# This script takes a setof effect sizes and test vector and computes Q
+
+args=commandArgs(TRUE)
+
+suppressWarnings(suppressMessages({
+  library(data.table)
+  library(dplyr)
+  library(Matrix)
+  library(pgenlibr)
+}))
+
+nfile = length(args)
+print(nfile)
+
+# Read in first file and collect all parameters
+out <- fread(args[1])[,2:10]
+tmp_name <- strsplit(args[1], "/")[[1]]
+out$rep <- tmp_name[4]
+out$gwas_size <- as.numeric(strsplit(tmp_name[5], "-")[[1]][2])
+out$test_size <- as.numeric(strsplit(tmp_name[6], "-")[[1]][2])
+out$h2 <- as.numeric(strsplit(tmp_name[7], "-")[[1]][2])
+out$num_causal <- as.numeric(strsplit(tmp_name[8], "-")[[1]][2])
+out$env <- as.numeric(strsplit(tmp_name[9], "_")[[1]][2])
+out$pheno <- tmp_name[10]
+out$test <- tmp_name[11]
+
+# Loops through all other files and add onto output
+for (i in 2:(nfile-1)) {
+
+  tmp <- fread(args[i])[, 2:10]
+  tmp_name <- strsplit(args[i], "/")[[1]]
+  rep$rep <- tmp_name[4]
+  rep$gwas_size <- as.numeric(strsplit(tmp_name[5], "-")[[1]][2])
+  rep$test_size <- as.numeric(strsplit(tmp_name[6], "-")[[1]][2])
+  rep$h2 <- as.numeric(strsplit(tmp_name[7], "-")[[1]][2])
+  rep$num_causal <- as.numeric(strsplit(tmp_name[8], "-")[[1]][2])
+  rep$env <- as.numeric(strsplit(tmp_name[9], "_")[[1]][2])
+  rep$pheno <- tmp_name[10]
+  rep$test <- tmp_name[11]
+  out <- rbind(out, tmp)
+
+}
+
+# Save output
+fwrite(out, args[nfile] ,row.names=T,quote=F,sep="\t", col.names = T)
+
+
