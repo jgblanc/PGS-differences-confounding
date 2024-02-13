3ae95bc8bca185643d2e18e7430953a248af5ce6
jgblanc <jgblanc@uchicago.edu>
2024-02-13 14:15:11 -0800

    t

---
+++ code/PRS/summarize_prs_SimpleGrid.R
@@ -0,0 +1,41 @@
+# This script averages polygenic scores over replications
+
+args=commandArgs(TRUE)
+
+suppressWarnings(suppressMessages({
+  library(data.table)
+  library(dplyr)
+}))
+
+
+outfile = args[1]
+popfile = args[2]
+nfile = length(args) - 2
+print(nfile)
+
+# Read in popfile
+pops <- fread(popfile)
+colnames(pops) <- c("IID","FID","Pop", "Lat", "Long")
+
+# Read in all PRS and combine with popfile
+df <- fread(args[3])
+colnames(df)[1] <- "IID"
+df <- inner_join(df, pops)
+
+# Add the rest of the files
+for (i in 4:length(args)) {
+
+  tmp <- fread(args[i])
+  colnames(tmp)[1] <- "IID"
+  tmp <- inner_join(tmp, pops)
+
+  df <- rbind(df, tmp)
+}
+df <- df %>% select(SCORE1_SUM, Pop, Lat, Long)
+
+# Compute the average
+dfOut <- df %>% mutate(nc_stan = (SCORE1_SUM - mean(SCORE1_SUM)) / sd(SCORE1_SUM)) %>% group_by(Pop) %>% summarise(avg_PRS = mean(nc_stan), LAT = mean(Lat), LONG = mean(Long))
+
+# Save output
+fwrite(dfOut, outfile ,row.names=F,quote=F,sep="\t", col.names = T)
+
