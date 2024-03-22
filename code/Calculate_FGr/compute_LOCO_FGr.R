22e0d0625e0a8ae4b37c31ae3034ea8c3c8e091f
jgblanc <jgblanc@uchicago.edu>
2024-03-22 16:33:10 -0500

    t

--- code/Calculate_FGr/compute_LOCO_FGr.R
+++ code/Calculate_FGr/compute_LOCO_FGr.R
@@ -12,43 +12,48 @@ rep = args[1]
 outfile = args[2]
 
 # Get rep number
-repNum = as.numeric(strsplit(rep, "")[[1]][2])
+repNum = as.numeric(strsplit(rep, "A")[[1]][2])
+print(repNum)
 
 # Read in target FGr
 covars <- fread(args[2 + repNum])[,1:4]
 M <- nrow(covars)
+print(head(covars))
 
 # Compute average FGr per deme over other reps
 dfAvg <- matrix(NA,nrow = 36, ncol = 99)
 x <- seq(1, 100)
 x <- x[-repNum]
+print(x)
 for(r in 1:99) {
 
-  print(paste0("r ", i))
 
   i <- x[r]
+  print(i)
 
   # Read in covar file and take mean per deme
-  tmp <- fread(args[2 + i])[,1:4]
+  id <- 2 + i
+  tmp <- fread(args[id])[,1:4]
   tmpAvg <- tmp %>% group_by(POP) %>% summarise(avgFGr = mean(FGr))
 
   # add to output list
-  dfAvg[,r] <- tmpAvg
+  dfAvg[,r] <- tmpAvg$avgFGr
 
 }
 
 # Compute average across rows
-tildeFGr <- rowMeans(tmpAvg)
+tildeFGr <- rowMeans(dfAvg)
 print(tildeFGr)
 
 # Add column to covars file
 covars$tildeFGr <- 0
 for (i in 1:nrow(covars)) {
 
-  popID <- covars[i,3]
-  covars[,5] <- tildeFGr[popID + 1]
+  popID <- as.numeric(covars[i,3])
+  covars[i,5] <- tildeFGr[popID + 1]
 
 }
+print(head(covars))
 
 
 # Save output
