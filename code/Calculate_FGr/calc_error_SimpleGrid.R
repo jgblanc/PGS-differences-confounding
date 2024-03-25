ac141caa1b191b93d4cccad0cb0733cdd273af0a
jgblanc <jgblanc@uchicago.edu>
2024-03-25 09:43:33 -0500

    t

--- code/Calculate_FGr/calc_error_SimpleGrid.R
+++ code/Calculate_FGr/calc_error_SimpleGrid.R
@@ -22,32 +22,40 @@ dfCovar <- fread(covar_file)
 
 
 # Find (1 - b2)
-find_b2 <- function(x,y){
-  return(as.numeric(b2))
+find_b2_one <- function(x,y){
+  dot.prod <- t(x)%*%y
+  norm.x <- norm(x,type="2")
+  norm.y <- norm(y,type="2")
+  b2 = 1-(dot.prod / (norm.x * norm.y))^2
+  return(b2)
 }
 
 # Find (1 - b2) - mulitple vectors
-find_b2 <- function(x,y){
+find_b2_many <- function(x,y){
   tmp <- rep(0, ncol(y))
+  y <- as.matrix(y)
   for (i in 1:ncol(y)) {
-    y <- scale(y)
-    tmp[i] <- (t(x)%*%y)^2
+    k <- y[,i]
+    dot.prod <- t(x)%*%k
+    norm.x <- norm(x,type="2")
+    norm.k <- norm(k,type="2")
+    tmp[i] = (dot.prod / (norm.x * norm.k))^2
   }
   b2 = 1-sum(tmp)
   return(as.numeric(b2))
 }
 
 # Find angle
-errorFGr <- find_b2(scale(dfTilde$tildeFGr), scale(dfCovar$FGr))
-error10 <- find_b2(scale(dfTilde$tildeFGr), dfCovar[,6:15])
-error35 <- find_b2(scale(dfTilde$tildeFGr), dfCovar[,6:40])
-error100 <- find_b2(scale(dfTilde$tildeFGr), dfCovar[,6:150])
+errorFGr <- find_b2_one(dfTilde$tildeFGr, dfCovar$FGr)
+error10 <- find_b2_many(dfTilde$tildeFGr, dfCovar[,6:15])
+error35 <- find_b2_many(dfTilde$tildeFGr, dfCovar[,6:40])
+error100 <- find_b2_many(dfTilde$tildeFGr, dfCovar[,6:150])
 
 
 
 
 # Save output
-out <- matrix(NA, nrow =1, ncol=5)
+out <- matrix(NA, nrow =1, ncol=4)
 out[1,] <- c(errorFGr, error10, error35, error100)
 out <- as.data.frame(out)
 colnames(out) <- c("FGr", "10", "35", "100")
