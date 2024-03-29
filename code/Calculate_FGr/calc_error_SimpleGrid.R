e92465f71b7d78eb104eea887978e6b47a8bb4b8
jgblanc <jgblanc@uchicago.edu>
2024-03-28 19:56:46 -0500

    t

--- code/Calculate_FGr/calc_error_SimpleGrid.R
+++ code/Calculate_FGr/calc_error_SimpleGrid.R
@@ -1,73 +1,153 @@
-# This script computes the error in either FGr or PCs for Simple Grid
-
+# This script computes an estimate of \tilde{F}_{Gr}
 args=commandArgs(TRUE)
 
-if(length(args)<3){stop("Rscript calc_FGr.R <tilde Fgr file> <covars file> <outfile> ")}
+if(length(args)<2){stop("Rscript compute_LOCO_FGr.R <FGr file> <outfile> ")}
 
 suppressWarnings(suppressMessages({
   library(data.table)
   library(dplyr)
 }))
 
-tFGr_file = args[1]
-covar_file = args[2]
-out_file = args[3]
+pc_num <- 2
+outfile = args[1]
 
-
-# Read in Tilde FGr
-dfTilde <- fread(tFGr_file)
-
-# Read in covar file
-dfCovar <- fread(covar_file)
-
-# Find (1 - b2)
-find_b2_FGr <- function(x,y){
-
-  y <- as.matrix(y)
-  x <- as.matrix(x)
-
-  # Compute variance explained
-  r <- cov(x,y) / sqrt(var(x), var(y))
-  r2 <- r^2
-
-  error = 1-r2
-  return(error)
+### Read in all covar files ###
+rep_files <- list()
+for (rep in 2:length(args)) {
+  covar <- fread(args[rep])
+  rep_files[[rep-1]] <- covar
 }
 
-# Find (1 - b2)
-find_b2_PCs <- function(x,y){
+### Compute Error in FGr ###
 
-  y <- as.matrix(y)
-  x <- as.matrix(x)
-  # Compute variance explained
-  mod <- lm(x ~ y)
-  r2 <- cor(x, fitted(mod))^2
-  b2 = 1-r2
-  return(b2)
+## Compute variance within demes
+avg_sigma_per_rep <- rep(0, 100)
+for (r in 1:100) {
+
+  print(r)
+  covar <- rep_files[[r]]
+
+  # Calc variance per deme
+  sigma_j <- rep(0, 36)
+  for (j in 0:35) {
+
+    # Select only individuals from a single deme
+    dfTmp <- covar %>% filter(POP == j)
+    FGr <- dfTmp$FGr
+    n <- nrow(dfTmp)
+
+    # Compute mean
+    FGrBar <- mean(FGr)
+
+    # For each deme compute sample variance
+    sigma_j[j+1] <- sum((FGr - FGrBar)^2) * (1/(n - 1))
+
+  }
+  # Take the average variance within demes
+  avg_sigma_per_rep[r] <- mean(sigma_j)
+}
+
+## Compute variance of means
+
+# Find mean per deme across reps
+deme_means <- matrix(NA, nrow = 36, ncol = 100)
+for (r in 1:100) {
+
+  # Find mean FGr per deme per replicate
+  tmp <- rep_files[[r]]
+  tmp_sum <- tmp %>% group_by(POP) %>% summarise(meanDeme = mean(FGr))
+  deme_means[, r] <- tmp_sum$meanDeme
+
+}
+meanDeme <- rowMeans(deme_means)
+
+# Compute variance of deme means across replcates
+deme_vars <- matrix(NA, nrow = 36, ncol = 100)
+for (r in 1:100) {
+
+  tmp <- rep_files[[r]]
+  tmp_sum <- tmp %>% group_by(POP) %>% summarise(meanDeme = mean(FGr))
+  deme_vars[,r] <-  (tmp_sum$meanDeme - meanDeme)^2
+
+}
+variance_deme_means <- rowSums(deme_vars) * (1/35) # check 35 or 36
+
+## Compute emprical variance of target vector
+empricalVar <- rep(0,100)
+for (r in 1:100) {
+  tmp <- rep_files[[r]]
+  empricalVar[r] <- var(tmp$FGr)
 }
 
 
+## Compute Error per replicate
+Error <- rep(0,100)
+for (r in 1:100) {
+  Error[r] <- (mean(avg_sigma_per_rep) + mean(variance_deme_means)) / empricalVar[r]
+}
+FGrError <- mean(Error)
 
-# Find angle
-#errorFGr <- find_b2(dfTilde$tildeFGr, dfCovar$FGr)
-#error10 <- find_b2(dfTilde$tildeFGr, dfCovar[,6:15])
-#error35 <- find_b2(dfTilde$tildeFGr, dfCovar[,6:40])
-#error100 <- find_b2(dfTilde$tildeFGr, dfCovar[,6:105])
+### Compute Lower Bound Error in PCs ###
 
-errorFGr <- find_b2(dfTilde$FGr, dfCovar$FGr)
-error10 <- find_b2(dfTilde$FGr, dfCovar[,6:15])
-error35 <- find_b2(dfTilde$FGr, dfCovar[,6:40])
-error100 <- find_b2(dfTilde$FGr, dfCovar[,6:105])
+## Compute variance within demes
+avg_sigma_per_rep <- matrix(NA, nrow = pc_num, ncol = 100)
+for (pc in 1:pc_num) {
 
+  print(paste0("PC ", pc))
+
+  for (r in 1:100) {
+
+    covar <- rep_files[[r]]
+
+    # Calc variance per deme
+    sigma_j <- rep(0, 36)
+    for (j in 0:35) {
+
+      # Select only individuals from a single deme
+      dfTmp <- covar %>% filter(POP == j)
+      colName <- paste0("PC",pc)
+      hatVec <- as.matrix(dfTmp[,..colName])
+      n <- nrow(dfTmp)
+
+      # Compute mean
+      hatVecBar <- mean(hatVec)
+
+      # For each deme compute sample variance
+      sigma_j[j+1] <- sum((hatVec - hatVecBar)^2) * (1/(n - 1))
+
+    }
+    # Take the average variance within demes
+    avg_sigma_per_rep[pc, r] <- mean(sigma_j)
+  }
+}
+
+## Compute emprical variance of target vector
+empricalVar <- matrix(NA, nrow=pc_num, ncol=100)
+for (pc in 1:pc_num) {
+  for (r in 1:100) {
+    tmp <- rep_files[[r]]
+    colName <- paste0("PC",pc)
+    hatVec <- as.matrix(tmp[,..colName])
+    empricalVar[pc, r] <- var(hatVec)[1,1]
+  }
+}
+
+## Compute Error
+Error_PC <- matrix(NA, nrow=pc_num, ncol=100)
+for (pc in 1:pc_num) {
+  for (r in 1:100) {
+    Error_PC[pc, r] <- (rowMeans(avg_sigma_per_rep)[pc]) / empricalVar[r]
+  }
+}
+rowMeans(Error_PC)
 
 
 # Save output
-out <- matrix(NA, nrow =1, ncol=4)
-out[1,] <- c(errorFGr, error10, error35, error100)
-out <- as.data.frame(out)
-colnames(out) <- c("FGr", "10", "35", "100")
-print(out)
-fwrite(out, out_file, row.names = F, col.names = T, quote = F, sep = "\t")
+dfOut <- as.data.frame(c(FGrError, rowMeans(Error_PC)))
+colnames(dfOut) <- "Error"
+dfOut$Type <- c("FGr", paste0("PC", seq(1,pc_num)))
+
+fwrite(dfOut, "../data/SimpleGrid_Error/L-20000_Error.txt", col.names = T, row.names = F, quote = F)
 
 
 
